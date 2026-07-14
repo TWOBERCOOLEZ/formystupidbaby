ea1b3fdfe1ad0ffc1795f603ee5322c4baf63a41
TWOBERCOOLEZ <aladyshevamilana@gmail.com>
2026-07-14 16:50:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,78 @@ start.style.display="none";
 gift.classList.remove("hidden");
 
 
+createBurst();
+
+
 },700);
+
+
+}
+
+
+
+function createBurst(){
+
+
+const present =
+document.querySelector(".present");
+
+
+const things = [
+"🍓",
+"❤️"
+];
+
+
+for(let i=0;i<18;i++){
+
+
+let item =
+document.createElement("div");
+
+
+item.className="burst";
+
+
+item.innerHTML =
+things[Math.floor(Math.random()*things.length)];
+
+
+
+item.style.left="75px";
+
+item.style.top="40px";
+
+
+
+item.style.setProperty(
+"--x",
+(Math.random()*300-150)+"px"
+);
+
+
+
+item.style.setProperty(
+"--y",
+(Math.random()*-250-50)+"px"
+);
+
+
+
+present.appendChild(item);
+
+
+
+setTimeout(()=>{
+
+item.remove();
+
+},2000);
+
+
+
+}
+
 
 
 }
